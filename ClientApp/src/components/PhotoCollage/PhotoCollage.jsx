5eeca2b43742c6106b97b80aa4e262ec2516c78f
KristianBakov <kristian.bakov@yahoo.com>
2024-06-06 13:22:28 +0100

    Fixed photo colalge modal to show only clicke diamge and cycle with arrows

--- ClientApp/src/components/PhotoCollage/PhotoCollage.jsx
+++ ClientApp/src/components/PhotoCollage/PhotoCollage.jsx
@@ -4,11 +4,27 @@ import "./PhotoCollage.css";
 
 const PhotoCollage = ({ photos }) => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   if (!photos || photos.length === 0) return null;
 
-  const handleOpenModal = () => setModalOpen(true);
+  const handleOpenModal = (index) => {
+    setCurrentIndex(index);
+    setModalOpen(true);
+  };
   const handleCloseModal = () => setModalOpen(false);
+
+  const showPrevImage = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? photos.length - 1 : prevIndex - 1
+    );
+  };
+
+  const showNextImage = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === photos.length - 1 ? 0 : prevIndex + 1
+    );
+  };
 
   const topRowPhotoCount = 1;
   const bottomRowPhotoCount = 4;
@@ -16,50 +32,63 @@ const PhotoCollage = ({ photos }) => {
   return (
     <div className="photo-collage">
       <div className="top-photo-container">
-        <img src={photos[0].source} alt={`Collage 0`} className="top-photo" />
+        <img
+          src={photos[0].source}
+          alt={`Collage 0`}
+          className="top-photo"
+          onClick={() => handleOpenModal(0)}
+        />
       </div>
       {photos.length > 1 && (
         <div className="bottom-row">
-          {photos.slice(topRowPhotoCount, 4).map((photo, index) => (
-            <div
-              key={index + topRowPhotoCount}
-              className="bottom-photo-container"
-            >
-              <img
-                src={photo.source}
-                alt={`Collage ${index + topRowPhotoCount}`}
-                className="bottom-photo"
-                onClick={
-                  photos.length > bottomRowPhotoCount &&
-                  index === topRowPhotoCount + 1
-                    ? handleOpenModal
-                    : undefined
-                }
-              />
-              {photos.length > bottomRowPhotoCount &&
-                index === topRowPhotoCount + 1 && (
-                  <div className="overlay" onClick={handleOpenModal}>
-                    +{photos.length - bottomRowPhotoCount}
-                  </div>
-                )}
-            </div>
-          ))}
+          {photos
+            .slice(topRowPhotoCount, bottomRowPhotoCount)
+            .map((photo, index) => (
+              <div
+                key={index + topRowPhotoCount}
+                className="bottom-photo-container"
+              >
+                <img
+                  src={photo.source}
+                  alt={`Collage ${index + topRowPhotoCount}`}
+                  className="bottom-photo"
+                  onClick={() => handleOpenModal(index + topRowPhotoCount)}
+                />
+                {photos.length > bottomRowPhotoCount &&
+                  index === bottomRowPhotoCount - 1 && (
+                    <div
+                      className="overlay"
+                      onClick={() => handleOpenModal(index + topRowPhotoCount)}
+                    >
+                      +{photos.length - bottomRowPhotoCount}
+                    </div>
+                  )}
+              </div>
+            ))}
         </div>
       )}
 
-      <Modal isOpen={modalOpen} toggle={handleCloseModal}>
-        <ModalHeader toggle={handleCloseModal}>Photo Gallery</ModalHeader>
+      <Modal
+        isOpen={modalOpen}
+        toggle={handleCloseModal}
+        className="photo-modal"
+      >
+        <ModalHeader toggle={handleCloseModal}></ModalHeader>
         <ModalBody>
           <div className="modal-collage">
-            {photos.map((photo, index) => (
-              <div key={index} className="modal-photo-container">
-                <img
-                  src={photo.source}
-                  alt={`Modal Collage ${index}`}
-                  className="modal-photo"
-                />
-              </div>
-            ))}
+            <button className="modal-arrow left" onClick={showPrevImage}>
+              &#9664;
+            </button>
+            <div className="modal-photo-container">
+              <img
+                src={photos[currentIndex].source}
+                alt={`Modal Collage ${currentIndex}`}
+                className="modal-photo"
+              />
+            </div>
+            <button className="modal-arrow right" onClick={showNextImage}>
+              &#9654;
+            </button>
           </div>
         </ModalBody>
       </Modal>
