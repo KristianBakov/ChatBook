23e48acceaae5f0c91047f2e30367f203d4def86
KristianBakov <kristian.bakov@yahoo.com>
2024-06-21 16:17:04 +0100

    Fixed button styles on profile

--- ClientApp/src/components/Routes/Profile/ProfileHeader/ProfileHeader.jsx
+++ ClientApp/src/components/Routes/Profile/ProfileHeader/ProfileHeader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Button } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import ButtonTabGroup from "../../../ButtonGroups/ButtonTabGroup/ButtonTabGroup";
 import ProfileUserFriendIconRow from "./ProfileUserFriendIconRow/ProfileUserFriendIconRow";
 import IconTextButton from "../../../Buttons/TextButton/IconTextButton";
